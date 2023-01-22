b4bc89bd212031f1d270eb4fc839730fecf427b7
Bruna <brunadias_30@hotmail.com>
2023-01-22 20:18:31 -0300

    seção populares

--- src/componentes/Populares/index.jsx
+++ src/componentes/Populares/index.jsx
@@ -7,8 +7,15 @@ export default function Populares() {
     <aside className={styles.populares}>
       <h2>Populares</h2>
       <ul className={styles.populares__imagens}>
-
+        {fotosPopulares.map((fotoPopular) => {
+          return (
+            <li key={fotoPopular.id}>
+              <img src={fotoPopular.path} alt={fotoPopular.alt} />
+            </li>
+          )
+        })}
       </ul>
+      <button> Ver mais fotos</button>
     </aside>
   )
 }
